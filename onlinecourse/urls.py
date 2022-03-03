d0f6152be82e29df2a64961aabb8cc56087335ac
Nirjhar Lo <hi@nirjharlo.com>
2022-03-03 22:53:14 +0530

    collectstatic enable

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     # <HINT> Create a route for show_exam_result view
     path('course/<int:course_id>/submission/<int:submission_id>/result', views.show_exam_result, name='show_exam_result'),
 
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
